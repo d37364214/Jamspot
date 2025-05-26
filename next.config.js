4ba1e6b62f7c00b246724269f1dbf20aece45d56
d37364214 <d37364214@gmail.com>
2025-05-26 16:01:41 +0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,20 @@
-const path = require('path');
+import path from 'path';
 
-      module.exports = {
-        experimental: {
-          appDir: true,
-        },
-        // Configuration pour servir les fichiers statiques de dist/public
-        outputFileTracing: false,
-        async rewrites() {
-          return [
-            {
-              source: '/public/:path*',
-              destination: path.resolve(__dirname, 'dist/public/:path*'),
-            },
-          ];
-        },
-      };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  // Configuration pour servir les fichiers statiques de dist/public
+  outputFileTracing: false,
+  async rewrites() {
+    return [
+      {
+        source: '/public/:path*',
+        destination: path.resolve(process.cwd(), 'dist/public/:path*'),
+      },
+    ];
+  },
+};
+
+export default nextConfig;
